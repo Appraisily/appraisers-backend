0fd3e662940dc2d16a2a8f20d929d6c798a2a384
Appraisily <134934621+Appraisily@users.noreply.github.com>
2024-10-31 08:13:39 +0100

    Update updatePendingAppraisal.js

--- appraisers-backend/routes/updatePendingAppraisal.js
+++ appraisers-backend/routes/updatePendingAppraisal.js
@@ -1,1 +1,10 @@
 
+// routes/updatePendingAppraisal.js
+
+const express = require('express');
+const router = express.Router();
+const updatePendingAppraisalController = require('../controllers/updatePendingAppraisalController');
+
+router.post('/update-pending-appraisal', updatePendingAppraisalController.updatePendingAppraisal);
+
+module.exports = router;
