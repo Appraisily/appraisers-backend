c5e9771dbd2073c26523001bbbfc12b17cbe4741
Appraisily <134934621+Appraisily@users.noreply.github.com>
2024-11-01 00:10:14 +0100

    Update auth.js

--- appraisers-backend/routes/auth.js
+++ appraisers-backend/routes/auth.js
@@ -1,13 +1,10 @@
-
 // routes/auth.js
 
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const authenticate = require('../middleware/authenticate');
 
-router.post('/authenticate', authController.authenticate);
-router.post('/logout', authController.logout);
-router.get('/check-auth', authenticate, authController.checkAuth);
+router.post('/authenticate', authController.authenticateUser);
+router.post('/logout', authController.logoutUser);
 
 module.exports = router;
