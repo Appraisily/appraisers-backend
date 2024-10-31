6526d88d0e8d0ba7b8f5e94b44500ce59267aabd
Appraisily <134934621+Appraisily@users.noreply.github.com>
2024-10-31 08:14:07 +0100

    Update authenticate.js

--- appraisers-backend/middleware/authenticate.js
+++ appraisers-backend/middleware/authenticate.js
@@ -1,1 +1,29 @@
 
+// middleware/authenticate.js
+
+const jwt = require('jsonwebtoken');
+const authorizedUsers = require('../shared/authorizedUsers');
+const { config } = require('../shared/config');
+
+module.exports = function authenticate(req, res, next) {
+  const token = req.cookies.jwtToken;
+
+  if (!token) {
+    return res.status(401).json({ success: false, message: 'Unauthorized. Token not provided.' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.JWT_SECRET);
+    req.user = decoded;
+
+    // Verificar si el usuario está en la lista de autorizados
+    if (!authorizedUsers.includes(decoded.email)) {
+      return res.status(403).json({ success: false, message: 'Forbidden. You do not have access to this resource.' });
+    }
+
+    next();
+  } catch (error) {
+    console.error('Error verifying JWT:', error);
+    res.status(401).json({ success: false, message: 'Invalid token.' });
+  }
+};
