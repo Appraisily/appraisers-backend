e1ccd4afd4d00991aec5bb87ac76b53baa34d267
Appraisily <134934621+Appraisily@users.noreply.github.com>
2024-11-01 00:38:51 +0100

    Update index.js

--- appraisers-backend/index.js
+++ appraisers-backend/index.js
@@ -2,21 +2,27 @@
 
 const express = require('express');
 const app = express();
+const cors = require('cors'); // Importar cors
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth');
 const appraisalsRoutes = require('./routes/appraisals');
-const { config } = require('./shared/config');
 
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
 
+// Configurar CORS
+app.use(cors({
+  origin: 'https://appraisers-frontend-856401495068.us-central1.run.app', // Reemplaza con la URL de tu frontend
+  credentials: true, // Permitir el envío de cookies y cabeceras de autorización
+}));
+
 // Rutas
 app.use('/api', authRoutes);
 app.use('/api', appraisalsRoutes);
 
-// Iniciar el servidor en el puerto proporcionado por Cloud Run
-const PORT = process.env.PORT || 8080; // Usa 8080 como valor predeterminado
+// Iniciar el servidor
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
