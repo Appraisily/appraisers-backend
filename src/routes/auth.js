8f00e5f746ad29571f5d22786be0149245334811
Appraisily <134934621+Appraisily@users.noreply.github.com>
2024-11-20 12:18:03 +0100

    aa

--- src/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AuthController = require('../controllers/authController');
-
-router.post('/login', AuthController.authenticateUser);
-router.post('/logout', AuthController.logoutUser);
-
-module.exports = router;
