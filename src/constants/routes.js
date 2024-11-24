570e3b574c27dab539f0178723d25f4db31d0d68
Appraisily <134934621+Appraisily@users.noreply.github.com>
2024-11-24 17:13:58 +0100

    asd

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -11,23 +11,9 @@ const API_ROUTES = {
     DETAILS_EDIT: 'appraisals/:id/list-edit',
     SET_VALUE: 'appraisals/:id/set-value',
     COMPLETE_PROCESS: 'appraisals/:id/complete-process',
-    PROCESS_WORKER: 'appraisals/process-worker',
-    UPDATE_PENDING: 'update-pending-appraisal'
-  }
+    PROCESS_WORKER: 'appraisals/process-worker'
+  },
+  UPDATE_PENDING: 'update-pending-appraisal'
 };
 
-const routeHelpers = {
-  getFullPath: (route) => `/api/${route.replace(/^\/+/, '')}`,
-  withId: (route, id) => route.replace(':id', id),
-  validatePath: (path) => {
-    const normalizedPath = path.replace(/^\/+|\/+$/g, '');
-    return Object.values(API_ROUTES).some(group => 
-      Object.values(group).includes(normalizedPath)
-    );
-  }
-};
-
-module.exports = { 
-  API_ROUTES,
-  routeHelpers
-};
+module.exports = { API_ROUTES };
--- src/middleware/routeValidator.js
+++ src/middleware/routeValidator.js
@@ -1,11 +1,12 @@
-const { API_ROUTES, routeHelpers } = require('../constants/routes');
+const { API_ROUTES } = require('../constants/routes');
 
 class RouteValidator {
   static validateRoutes(router) {
     const routes = this.getRoutes(router);
     
     routes.forEach(route => {
-      if (!routeHelpers.validatePath(route.path)) {
+      const normalizedPath = this.normalizePath(route.path);
+      if (!this.isValidRoute(normalizedPath)) {
         throw new Error(`Route ${route.path} is not defined in API_ROUTES`);
       }
     });
@@ -23,12 +24,47 @@ class RouteValidator {
           methods: Object.keys(layer.route.methods)
         });
       } else if (layer.name === 'router') {
-        const newPrefix = prefix + (layer.regexp.source === '/' ? '' : layer.regexp.source);
+        const newPrefix = prefix + (layer.regexp.source === '/' ? '' : this.cleanPath(layer.regexp.source));
         routes.push(...this.getRoutes(layer.handle, newPrefix));
       }
     });
 
     return routes;
+  }
+
+  static normalizePath(path) {
+    return path
+      .replace(/^\/+|\/+$/g, '') // Remove leading/trailing slashes
+      .replace(/\/+/g, '/') // Remove duplicate slashes
+      .replace(/:\w+/g, ':id'); // Normalize params to :id
+  }
+
+  static cleanPath(path) {
+    return path
+      .replace(/^\^\\\//, '') // Remove leading ^\/
+      .replace(/\\\/\?\(\?\=.*$/, '') // Remove trailing regex
+      .replace(/\\\//g, '/') // Replace \/ with /
+      .replace(/^\^|\$$/g, '') // Remove ^ and $ anchors
+      .replace(/\(\?:\([^\)]+\)\)\?/g, '') // Remove optional groups
+      .replace(/\([^\)]+\)/g, '') // Remove remaining groups
+      .replace(/\\(.)/g, '$1'); // Unescape characters
+  }
+
+  static isValidRoute(path) {
+    // Get all defined routes
+    const routes = [];
+    Object.values(API_ROUTES).forEach(group => {
+      if (typeof group === 'string') {
+        routes.push(this.normalizePath(group));
+      } else {
+        Object.values(group).forEach(route => {
+          routes.push(this.normalizePath(route));
+        });
+      }
+    });
+
+    // Check if normalized path matches any defined route
+    return routes.includes(path);
   }
 }
 
