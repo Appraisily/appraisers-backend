2e793ec956346bca3d45676fdc35ad07b3715309
Andrés Gómez Rodríguez <34994419b@csic.gal>
2025-02-19 10:13:37 +0100

    AA

--- appraisers-backend/src/routes/updatePendingAppraisal.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const UpdatePendingAppraisalController = require('../controllers/updatePendingAppraisalController');
-
-router.post('/', UpdatePendingAppraisalController.updatePendingAppraisal);
-
-module.exports = router;
